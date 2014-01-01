fc0be671db87758463e8695bf946fb24aab1b8fa
sahil shekhawat <sahilshekhawat01@gmail.com>
2014-01-01 23:34:50 +0530

    added login.

--- app/views.py
+++ app/views.py
@@ -1,8 +1,16 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from google.appengine.api import users
 
 
 def index(request):
-    return render(request, 'index.html', {'title' : 'hackathon'})
+    user = users.get_current_user()
+    if user:
+        greetings = ('welcome, %s!' % (user.nickname()))
+        href = users.create_logout_url('/')
+    else:
+        greetings = ('You must LOGIN first!')
+        href = users.create_login_url('/')
+
+    return render(request, 'index.html', {'title' : 'hackathon' , 'greetings' : greetings , 'href' : href})
