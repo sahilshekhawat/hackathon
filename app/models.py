a855387bf2befb187a7f1b0d60f14cb846c3348d
sahil shekhawat <sahilshekhawat01@gmail.com>
2014-01-05 21:37:02 +0530

    added models for a user session.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,14 @@
+import datetime
 from google.appengine.ext import ndb
+from google.appengine.api import users
+
 
 class date(ndb.Model):
     date = ndb.DateTimeProperty(auto_now_add=True)
+
+
+
+class account(ndb.Model):
+    username = ndb.StringProperty()
+    user_id = ndb.IntegerProperty()
+    email = ndb.StringProperty()
